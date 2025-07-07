875fea1c3280efb847375c4c87c743554e9144ce
ashish6667 <ay1060045@gmail.com>
2025-07-07 17:01:28 +0530

    refactor: simplify API base URL configuration in api.js

--- frontend/user info/src/utils/api.js
+++ frontend/user info/src/utils/api.js
@@ -1,11 +1,7 @@
-// utils/api.js
-import axios from "axios";
+import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL + "/users", // Make sure this is correct
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 export default API;
